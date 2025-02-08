e53650e3f77f6f92e2a21513ab3e9c0324ab0b5a
Antriksh Prasad <antrikshprasad@Antrikshs-MacBook-Air.local>
2025-02-08 18:15:24 +0530

    identation

--- cruddemo/src/main/java/com/demo/springboot/cruddemo/EmployeeService/EmployeeService.java
+++ cruddemo/src/main/java/com/demo/springboot/cruddemo/EmployeeService/EmployeeService.java
@@ -5,8 +5,13 @@ import com.demo.springboot.cruddemo.entity.Employee;
 import java.util.List;
 
 public interface EmployeeService {
+
     List<Employee> findAll();
+
     Employee findById(int theId);
+
     Employee save(Employee theEmployee);
+
     void deleteById(int theId);
+
 }
